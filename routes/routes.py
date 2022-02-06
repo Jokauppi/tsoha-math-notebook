48a0585a1bfc9c2503b6dcb2e34ce94b55de7b0a
Joona Kauppila <joona.kauppila@helsinki.fi>
2022-02-06 21:00:59 +0200

    add registration validation, add error page

--- routes/routes.py
+++ routes/routes.py
@@ -24,3 +24,18 @@ def logout():
 def register():
     if request.method == "GET":
         return render_template("register.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        if len(username) < 3 or len(username) > 20:
+            return render_template("error.html", redirect="/register", message="Username too short or long")
+
+        password = request.form["password1"]
+        password_again = request.form["password2"]
+
+        if len(password) < 4:
+            return render_template("error.html", redirect="/register", message="Password too short")
+
+        if password != password_again:
+            return render_template("error.html", redirect="/register", message="Passwords dont match")
+
