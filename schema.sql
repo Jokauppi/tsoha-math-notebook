93a5379e886aaa1484b0fd082931f115209a229b
Joona Kauppila <joona.kauppila@helsinki.fi>
2022-02-06 19:21:02 +0200

    init schema, rework package structure

---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    password TEXT
+);
+
+CREATE TABLE notebooks (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE pages (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    notebook_id INTEGER REFERENCES notebooks
+);
+
+CREATE TABLE equations (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    page_id INTEGER REFERENCES pages,
+    order, INTEGER
+);
