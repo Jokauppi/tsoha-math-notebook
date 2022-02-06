a2402cae7012eac74640a728fe499a5592810852
Joona Kauppila <joona.kauppila@helsinki.fi>
2022-02-06 20:39:09 +0200

    create login UI, init pylint

--- routes/routes.py
+++ routes/routes.py
@@ -3,4 +3,24 @@ from flask import render_template, request, redirect
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="Notebooks")
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    
+    # user & pass check  
+
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/register", methods=["get", "post"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
