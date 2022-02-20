2c705d1d8e92ce1bff6231326f97bfe2e206d646
Joona Kauppila <joona.kauppila@helsinki.fi>
2022-02-20 21:07:44 +0200

    fix page listing

--- schema.sql
+++ schema.sql
@@ -1,25 +1,26 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE,
-    password TEXT
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
 );
 
 CREATE TABLE notebooks (
     id SERIAL PRIMARY KEY,
-    title TEXT,
-    user_id INTEGER REFERENCES users
+    title TEXT NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users
 );
 
 CREATE TABLE pages (
     id SERIAL PRIMARY KEY,
-    title TEXT,
-    notebook_id INTEGER REFERENCES notebooks
+    title TEXT NOT NULL,
+    notebook_id INTEGER NOT NULL REFERENCES notebooks ON DELETE CASCADE,
+    modified DATE
 );
 
 CREATE TABLE equations (
     id SERIAL PRIMARY KEY,
     content TEXT,
-    page_id INTEGER REFERENCES pages,
+    page_id INTEGER NOT NULL REFERENCES pages ON DELETE CASCADE,
     order_num INTEGER,
     type VARCHAR(1)
 );
--- services/pages.py
+++ services/pages.py
@@ -3,16 +3,9 @@ from services import notebooks
 
 def create(title, notebook_id, user_id):
 
-    print("2")
-
     notebook = notebooks.get(notebook_id, user_id)
 
-    print("3")
-
-    print(notebook)
-
     if notebook:
-        print("4")
         try:
             sql = """INSERT INTO pages (title, notebook_id) VALUES (:title, :notebook_id)"""
             db.session.execute(sql, {"title":title, "notebook_id":notebook.id})
@@ -20,14 +13,13 @@ def create(title, notebook_id, user_id):
         except:
             return False
         return True
-    
 
     return False
 
-def get_all(notebook_id, user):
+def get_all(notebook_id, user_id):
     try:
-        sql = """SELECT id, title FROM pages WHERE user_id=:user AND notebook_id=:notebook_id"""
-        result = db.session.execute(sql, {"user":user, "notebook_id":notebook_id})
+        sql = """SELECT pages.id, pages.title FROM pages INNER JOIN notebooks ON notebook_id = notebooks.id WHERE user_id=:user AND notebook_id=:notebook_id"""
+        result = db.session.execute(sql, {"user":user_id, "notebook_id":notebook_id})
         notebooks = result.fetchall()
     except:
         return []
